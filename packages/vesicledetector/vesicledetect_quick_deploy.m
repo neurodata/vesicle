38470a79f96c01f252c9da5fdc9e44d0ea1fdcda
Will Gray Roncal <wgr@jhu.edu>
2015-11-15 10:24:22 -0500

    fixing deploy script

--- packages/vesicledetector/vesicledetect_quick_deploy.m
+++ packages/vesicledetector/vesicledetect_quick_deploy.m
@@ -12,16 +12,29 @@ load(queryFile)
 
 cube = oo.query(query);
 
-vesicledetect_quick(cube, template, annoId, annoId, neighborhood_size, neighbor_dist, thresh, data_set, padX, padY, padZ, outFile)
-
-% Put data
-cube.setChannel(upChannel);
-
-cube.setDataType(eRAMONChannelDataType.uint32); %just in case
-cube.setChannelType(eRAMONChannelType.annotation)
-tic
-oo.setAnnoToken(upToken)
-oo.setAnnoChannel(upChannel)
-oo.createAnnotation(cube);
-fprintf('Block Write Upload: ');
-toc
+if cube.data(:) > 0
+    zz = tempname;
+    
+    vesicledetect_quick(cube, template, annoId, neighborhood_size, neighbor_dist, thresh, data_set, padX, padY, padZ, zz)
+    
+    load(zz)
+    
+    delete(zz)
+    
+    if sum(cube.data(:)) > 0
+        % Put data
+        cube.setChannel(upChannel);
+        cube.setDataType(eRAMONChannelDataType.uint32); %just in case
+        cube.setChannelType(eRAMONChannelType.annotation)
+        tic
+        oo.setAnnoToken(upToken)
+        oo.setAnnoChannel(upChannel)
+        oo.createAnnotation(cube);
+        fprintf('Block Write Upload: ');
+        toc
+    else
+        disp('no vesicles - skipping upload')
+    end
+else
+    disp('black region - skipping processing')
+end
